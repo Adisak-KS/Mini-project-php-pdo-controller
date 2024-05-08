b58e8cb13a20b65f3cffeb309b0c0403268c8e49
Adisak-KS <adisak.general@gmail.com>
2024-05-08 20:21:44 +0700

    Add Folder previews

--- db/connect.php
+++ db/connect.php
@@ -1,22 +1,21 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "123";
-    $db = "employeesdb";
-    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "employeesdb";
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
-    try{
-        $pdo = new PDO($dsn, $username, $password);
-        // echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
-    }catch(PDOException $e){
-        echo $e->getMessage();
-    }
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    // echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
-    require_once "controller.php";
-    require_once "user.php";
+require_once "controller.php";
+require_once "user.php";
 
-    $controller = new Controller($pdo);
-    $user = new User($pdo);
+$controller = new Controller($pdo);
+$user = new User($pdo);
 
-    $user->insertUser('admin','123');
-?>
+$user->insertUser('admin', '123');
